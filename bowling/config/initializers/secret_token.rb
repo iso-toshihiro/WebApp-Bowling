f4d673b45b7d62995ac2d83309f31596b336bce2
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-20 15:28:14 +0900

    rails new bowling
    WEBアプリ作成開始

---
+++ bowling/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = 'e3e364deefe60e3a9bdaeb4c5de1090d3245685d5053d0fc71df449cc3fbdc910b15f9e7baebf40b7c4782d5abba492a99c563f51a3b7df1adcc208a0632ac6f'
