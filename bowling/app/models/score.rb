07bc6b6c85466d04279b99cedf186f0963fb99eb
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-26 19:31:57 +0900

    アクセスメソッドを追加

--- bowling/app/models/score.rb
+++ bowling/app/models/score.rb
@@ -1,3 +1,39 @@
 class Score < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  attr_accessible :player_name, :game_date,
+             :down_pins_1st_roll_1_frame,
+             :down_pins_2nd_roll_1_frame,
+             :down_pins_1st_roll_2_frame, 
+             :down_pins_2nd_roll_2_frame, 
+             :down_pins_1st_roll_3_frame, 
+             :down_pins_2nd_roll_3_frame, 
+             :down_pins_1st_roll_4_frame, 
+             :down_pins_2nd_roll_4_frame, 
+             :down_pins_1st_roll_5_frame, 
+             :down_pins_2nd_roll_5_frame, 
+             :down_pins_1st_roll_6_frame,  
+             :down_pins_2nd_roll_6_frame, 
+             :down_pins_1st_roll_7_frame, 
+             :down_pins_2nd_roll_7_frame, 
+             :down_pins_1st_roll_8_frame, 
+             :down_pins_2nd_roll_8_frame, 
+             :down_pins_1st_roll_9_frame, 
+             :down_pins_2nd_roll_9_frame, 
+             :down_pins_1st_roll_10_frame, 
+             :down_pins_2nd_roll_10_frame, 
+             :down_pins_3rd_roll_10_frame, 
+             :score_1_frame, 
+             :score_2_frame, 
+             :score_3_frame, 
+             :score_4_frame, 
+             :score_5_frame, 
+             :score_6_frame, 
+             :score_7_frame, 
+             :score_8_frame, 
+             :score_9_frame, 
+             :score_10_frame
+
+
+
 end
