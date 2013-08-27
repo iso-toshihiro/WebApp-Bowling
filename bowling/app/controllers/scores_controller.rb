32a9af1c099f951bcac0cb992113f79f43fe6a79
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-27 16:00:17 +0900

    createアクションを作成、confirmを修正、クラス変数の導入

--- bowling/app/controllers/scores_controller.rb
+++ bowling/app/controllers/scores_controller.rb
@@ -1,6 +1,7 @@
 require 'score_calcuration'
 
 class ScoresController < ApplicationController
+
   def index
     @scores = Score.all
   end
@@ -14,20 +15,62 @@ class ScoresController < ApplicationController
   def edit
   end
 
-  def create
-  end
-
   def confirm
     @name = params[:player_name]
+    @@name = params[:player_name]
     if params[:current_time] == "on"
       @date = Time.now
     else
       @date = params[:game_date]
     end
+    @@date = @date
     @down_pins = params[:down_pin]
-    get_scores = Score_calcuration.new (@down_pins)
+    @@down_pins = params[:down_pin]
+    get_scores = Score_calcuration.new (@@down_pins)
     get_scores.calcurate_scores
     @total = get_scores.scores
+    @@total = get_scores.scores
+  end
+
+  def create
+    Score.create( :player_name => @@name,
+                  :game_date => @@date,
+                  :down_pins_1st_roll_1_frame => @@down_pins[0],
+                  :down_pins_2nd_roll_1_frame => @@down_pins[1],
+                  :down_pins_1st_roll_2_frame => @@down_pins[2],
+                  :down_pins_2nd_roll_2_frame => @@down_pins[3], 
+                  :down_pins_1st_roll_3_frame => @@down_pins[4], 
+                  :down_pins_2nd_roll_3_frame => @@down_pins[5], 
+                  :down_pins_1st_roll_4_frame => @@down_pins[6], 
+                  :down_pins_2nd_roll_4_frame => @@down_pins[7], 
+                  :down_pins_1st_roll_5_frame => @@down_pins[8], 
+                  :down_pins_2nd_roll_5_frame => @@down_pins[9], 
+                  :down_pins_1st_roll_6_frame => @@down_pins[10],  
+                  :down_pins_2nd_roll_6_frame => @@down_pins[11], 
+                  :down_pins_1st_roll_7_frame => @@down_pins[12], 
+                  :down_pins_2nd_roll_7_frame => @@down_pins[13], 
+                  :down_pins_1st_roll_8_frame => @@down_pins[14], 
+                  :down_pins_2nd_roll_8_frame => @@down_pins[15], 
+                  :down_pins_1st_roll_9_frame => @@down_pins[16], 
+                  :down_pins_2nd_roll_9_frame => @@down_pins[17], 
+                  :down_pins_1st_roll_10_frame => @@down_pins[18], 
+                  :down_pins_2nd_roll_10_frame => @@down_pins[19], 
+                  :down_pins_3rd_roll_10_frame => @@down_pins[20], 
+                  :score_1_frame => @@total[0], 
+                  :score_2_frame => @@total[1], 
+                  :score_3_frame => @@total[2], 
+                  :score_4_frame => @@total[3], 
+                  :score_5_frame => @@total[4], 
+                  :score_6_frame => @@total[5], 
+                  :score_7_frame => @@total[6], 
+                  :score_8_frame => @@total[7], 
+                  :score_9_frame => @@total[8], 
+                  :score_10_frame => @@total[9] )
+
+    @date = @@date
+    @name = @@name
+    @down_pins = @@down_pins
+    @total = @@total
   end
 
   def update
