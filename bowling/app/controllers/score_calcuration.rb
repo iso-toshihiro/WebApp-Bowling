df3bb6f6832fe41da9ef2bb877d48ee079c5fbd9
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-27 11:40:07 +0900

    倒したピンの数をgetするクラスを作成

--- bowling/app/controllers/score_calcuration.rb
+++ bowling/app/controllers/score_calcuration.rb
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
-class Score_calcuration
-
-  attr_accessor :scores
+class Get_down_pin_number
 
   def initialize(down_pins)
-    @scores = []
     @down_pin_list = []
-    @bonus_point = 0 #ストライクとスペアのボーナス得点
     i = 0
     down_pins.each do |pins|  #整数値に変換
       @down_pin_list[i] = pins.to_i
       i += 1  
     end
-   
   end
-
+  
   def first_roll(frame)
     return @down_pin_list[2*frame-2]
   end
@@ -26,13 +21,25 @@ class Score_calcuration
   def third_roll(frame) #10フレーム目の3投目
     return @down_pin_list[2*frame]
   end
+
+end
+
+class Score_calcuration
+
+  attr_accessor :scores
+
+  def initialize(down_pins)
+    @scores = []
+    @bonus_point = 0 #ストライクとスペアのボーナス得点
+    @get = Get_down_pin_number.new (down_pins)
+  end
   
   def strike? (frame)
-    first_roll(frame) == 10
+    @get.first_roll(frame) == 10
   end
 
   def spare? (frame)
-    first_roll(frame) + second_roll(frame) == 10
+    @get.first_roll(frame) + @get.second_roll(frame) == 10
   end
 
   def calcurate_scores
@@ -48,30 +55,30 @@ class Score_calcuration
 
   def calcurate_1_9frame_score(frame)
     if strike?(frame) and strike?(frame+1) #次フレームもストライクの場合
-      @bonus_point = first_roll(frame+1) + first_roll(frame+2)
+      @bonus_point = @get.first_roll(frame+1) + @get.first_roll(frame+2)
       if frame == 9 #9フレーム目だけ特別
-        @bonus_point = first_roll(10) + second_roll(10)
+        @bonus_point = @get.first_roll(10) + @get.second_roll(10)
       end
     elsif strike?(frame) #ストライクの場合
-      @bonus_point = first_roll(frame+1) + second_roll(frame+1)
+      @bonus_point = @get.first_roll(frame+1) + @get.second_roll(frame+1)
     elsif spare?(frame) #スペアの場合
-      @bonus_point = first_roll(frame+1)
+      @bonus_point = @get.first_roll(frame+1)
     else
       @bonus_point = 0
     end
 
     if frame == 1
-      @scores[0] = first_roll(1) + second_roll(1) + @bonus_point
+      @scores[0] = @get.first_roll(1) + @get.second_roll(1) + @bonus_point
     else
-      @scores[frame-1] = @scores[frame-2] + first_roll(frame) + second_roll(frame) + @bonus_point
+      @scores[frame-1] = @scores[frame-2] + @get.first_roll(frame) + @get.second_roll(frame) + @bonus_point
     end
   end
   
   def calcurate_10frame_score
     if strike?(10) or spare?(10) 
-      @scores[9] = @scores[8] + first_roll(10) + second_roll(10) + third_roll(10)
+      @scores[9] = @scores[8] + @get.first_roll(10) + @get.second_roll(10) + third_roll(10)
     else
-      @scores[9] = @scores[8] + first_roll(10) + second_roll(10)
+      @scores[9] = @scores[8] + @get.first_roll(10) + @get.second_roll(10)
     end
   end
   
