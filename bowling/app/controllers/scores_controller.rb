2dc0d6076112a61ff5b12edad1aee47bbe9f0871
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-29 17:10:41 +0900

    リファクタリング　変数名変更

--- bowling/app/controllers/scores_controller.rb
+++ bowling/app/controllers/scores_controller.rb
@@ -13,51 +13,49 @@ class ScoresController < ApplicationController
 
   def new
     @from = 'new'
-    @name = 'ユーザー'
-    @date = '2013-08-25 12:00:00'
-    @pin_list = Array.new(21,0)
+    @player_name = 'ユーザー'
+    @game_date = '2013-08-25 12:00:00'
+    @down_pins = Array.new(21,0)
   end
 
   def edit
-    @scores = Score.find(params[:id])
-    @name = @scores.player_name
-    @date = @scores.game_date
+    @score_db_record = Score.find(params[:id])
+    @player_name = @score_db_record.player_name
+    @game_date = @score_db_record.game_date
     score_object = Score.new
-    @down_pins = score_object.get_down_pin_list(@scores)
-    @pin_list = score_object.get_down_pin_list(@scores)
-    @total = score_object.get_score_list(@scores)
+    @down_pins = score_object.get_down_pin_list(@score_db_record)
+    @score_list = score_object.get_score_list(@score_db_record)
     @from = 'edit'
     @id = params[:id]
   end
 
   def confirm
     @from = params[:from]
-    @name = params[:new_player_name]
+    @player_name = params[:new_player_name]
     if params[:current_time] == 'on'
-      @date = Time.now
+      @game_date = Time.now
     else
-      @date = params[:new_game_date]
+      @game_date = params[:new_game_date]
     end
-    @down_pins = params[:new_down_pin]
+    @down_pins = params[:new_down_pins]
     scores = ScoreCalculator.new (@down_pins)
-    scores.calculate_scores
-    @total = scores.scores
+    @score_list = scores.calculate_score_list
     if @from == 'edit'
       @id = params[:id]
-      @old_name = params[:old_player_name]
-      @old_date = params[:old_game_date]
-      @old_down_pins = params[:old_down_pin]
-      @old_total = params[:old_total]
+      @old_player_name = params[:old_player_name]
+      @old_game_date = params[:old_game_date]
+      @old_down_pins = params[:old_down_pins]
+      @old_score_list = params[:old_score_list]
     end
   end
 
   def create
-    @name = params[:player_name]
-    @date = params[:game_date]
-    @down_pins = params[:down_pin]
-    @total = params[:total]
-    Score.create( :player_name => @name,
-                  :game_date => @date,
+    @player_name = params[:player_name]
+    @game_date = params[:game_date]
+    @down_pins = params[:down_pins]
+    @score_list = params[:score_list]
+    Score.create( :player_name => @player_name,
+                  :game_date => @game_date,
                   :down_pins_1st_roll_1_frame => @down_pins[0],
                   :down_pins_2nd_roll_1_frame => @down_pins[1],
                   :down_pins_1st_roll_2_frame => @down_pins[2],
@@ -79,26 +77,26 @@ class ScoresController < ApplicationController
                   :down_pins_1st_roll_10_frame => @down_pins[18], 
                   :down_pins_2nd_roll_10_frame => @down_pins[19], 
                   :down_pins_3rd_roll_10_frame => @down_pins[20], 
-                  :score_1_frame => @total[0], 
-                  :score_2_frame => @total[1], 
-                  :score_3_frame => @total[2], 
-                  :score_4_frame => @total[3], 
-                  :score_5_frame => @total[4], 
-                  :score_6_frame => @total[5], 
-                  :score_7_frame => @total[6], 
-                  :score_8_frame => @total[7], 
-                  :score_9_frame => @total[8], 
-                  :score_10_frame => @total[9] )
+                  :score_1_frame => @score_list[0], 
+                  :score_2_frame => @score_list[1], 
+                  :score_3_frame => @score_list[2], 
+                  :score_4_frame => @score_list[3], 
+                  :score_5_frame => @score_list[4], 
+                  :score_6_frame => @score_list[5], 
+                  :score_7_frame => @score_list[6], 
+                  :score_8_frame => @score_list[7], 
+                  :score_9_frame => @score_list[8], 
+                  :score_10_frame => @score_list[9] )
   end
 
   def update
-    @name = params[:player_name]
-    @date = params[:game_date]
-    @down_pins = params[:down_pin]
-    @total = params[:total]
-    @score = Score.find(params[:id])
-    @score.update_attributes( :player_name => @name,
-                  :game_date => @date,
+    @player_name = params[:player_name]
+    @game_date = params[:game_date]
+    @down_pins = params[:down_pins]
+    @score_list = params[:score_list]
+    @score_db_record = Score.find(params[:id])
+    @score_db_record.update_attributes( :player_name => @player_name,
+                  :game_date => @game_date,
                   :down_pins_1st_roll_1_frame => @down_pins[0],
                   :down_pins_2nd_roll_1_frame => @down_pins[1],
                   :down_pins_1st_roll_2_frame => @down_pins[2],
@@ -120,16 +118,16 @@ class ScoresController < ApplicationController
                   :down_pins_1st_roll_10_frame => @down_pins[18], 
                   :down_pins_2nd_roll_10_frame => @down_pins[19], 
                   :down_pins_3rd_roll_10_frame => @down_pins[20], 
-                  :score_1_frame => @total[0], 
-                  :score_2_frame => @total[1], 
-                  :score_3_frame => @total[2], 
-                  :score_4_frame => @total[3], 
-                  :score_5_frame => @total[4], 
-                  :score_6_frame => @total[5], 
-                  :score_7_frame => @total[6], 
-                  :score_8_frame => @total[7], 
-                  :score_9_frame => @total[8], 
-                  :score_10_frame => @total[9] )
+                  :score_1_frame => @score_list[0], 
+                  :score_2_frame => @score_list[1], 
+                  :score_3_frame => @score_list[2], 
+                  :score_4_frame => @score_list[3], 
+                  :score_5_frame => @score_list[4], 
+                  :score_6_frame => @score_list[5], 
+                  :score_7_frame => @score_list[6], 
+                  :score_8_frame => @score_list[7], 
+                  :score_9_frame => @score_list[8], 
+                  :score_10_frame => @score_list[9] )
  
   end
 
