6792fb50d90fb2e06db49476cb9688c650876f8e
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-29 15:04:47 +0900

    updateの作成, confirm、newの修正

--- bowling/app/controllers/scores_controller.rb
+++ bowling/app/controllers/scores_controller.rb
@@ -23,22 +23,32 @@ class ScoresController < ApplicationController
     @name = @scores.player_name
     @date = @scores.game_date
     score_object = Score.new
+    @down_pins = score_object.get_down_pin_list(@scores)
     @pin_list = score_object.get_down_pin_list(@scores)
+    @total = score_object.get_score_list(@scores)
     @from = 'edit'
+    @id = params[:id]
   end
 
   def confirm
     @from = params[:from]
-    @name = params[:player_name]
+    @name = params[:new_player_name]
     if params[:current_time] == 'on'
       @date = Time.now
     else
-      @date = params[:game_date]
+      @date = params[:new_game_date]
     end
-    @down_pins = params[:down_pin]
+    @down_pins = params[:new_down_pin]
     scores = ScoreCalculator.new (@down_pins)
     scores.calculate_scores
     @total = scores.scores
+    if @from == 'edit'
+      @id = params[:id]
+      @old_name = params[:old_player_name]
+      @old_date = params[:old_game_date]
+      @old_down_pins = params[:old_down_pin]
+      @old_total = params[:old_total]
+    end
   end
 
   def create
@@ -82,6 +92,45 @@ class ScoresController < ApplicationController
   end
 
   def update
+    @name = params[:player_name]
+    @date = params[:game_date]
+    @down_pins = params[:down_pin]
+    @total = params[:total]
+    @score = Score.find(params[:id])
+    @score.update_attributes( :player_name => @name,
+                  :game_date => @date,
+                  :down_pins_1st_roll_1_frame => @down_pins[0],
+                  :down_pins_2nd_roll_1_frame => @down_pins[1],
+                  :down_pins_1st_roll_2_frame => @down_pins[2],
+                  :down_pins_2nd_roll_2_frame => @down_pins[3], 
+                  :down_pins_1st_roll_3_frame => @down_pins[4], 
+                  :down_pins_2nd_roll_3_frame => @down_pins[5], 
+                  :down_pins_1st_roll_4_frame => @down_pins[6], 
+                  :down_pins_2nd_roll_4_frame => @down_pins[7], 
+                  :down_pins_1st_roll_5_frame => @down_pins[8], 
+                  :down_pins_2nd_roll_5_frame => @down_pins[9], 
+                  :down_pins_1st_roll_6_frame => @down_pins[10],  
+                  :down_pins_2nd_roll_6_frame => @down_pins[11], 
+                  :down_pins_1st_roll_7_frame => @down_pins[12], 
+                  :down_pins_2nd_roll_7_frame => @down_pins[13], 
+                  :down_pins_1st_roll_8_frame => @down_pins[14], 
+                  :down_pins_2nd_roll_8_frame => @down_pins[15], 
+                  :down_pins_1st_roll_9_frame => @down_pins[16],
+                  :down_pins_2nd_roll_9_frame => @down_pins[17], 
+                  :down_pins_1st_roll_10_frame => @down_pins[18], 
+                  :down_pins_2nd_roll_10_frame => @down_pins[19], 
+                  :down_pins_3rd_roll_10_frame => @down_pins[20], 
+                  :score_1_frame => @total[0], 
+                  :score_2_frame => @total[1], 
+                  :score_3_frame => @total[2], 
+                  :score_4_frame => @total[3], 
+                  :score_5_frame => @total[4], 
+                  :score_6_frame => @total[5], 
+                  :score_7_frame => @total[6], 
+                  :score_8_frame => @total[7], 
+                  :score_9_frame => @total[8], 
+                  :score_10_frame => @total[9] )
+ 
   end
 
   def destroy
