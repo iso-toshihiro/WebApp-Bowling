27bf48f597afee55be2e0e3bb3e28b9db8f248c8
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-04 12:08:46 +0900

    リファクタリング　関数名、変数名修正

--- bowling/app/models/validation.rb
+++ bowling/app/models/validation.rb
@@ -4,37 +4,29 @@ class Validation
   attr_accessor :errors_pins_not_number
   attr_accessor :errors_pins_over_10
   attr_accessor :error_3rd_roll_10frame
-  attr_accessor :errors_10frame_2nd_3rd_roll
+  attr_accessor :error_10frame_2nd_3rd_roll
 
   def initialize(down_pins)
-    @down_pins                   = []
     @down_pins                   = down_pins
     @errors_pins_not_number      = [nil]
     @errors_pins_over_10         = [nil]
     @error_3rd_roll_10frame      = 0
-    @errors_10frame_2nd_3rd_roll = 0
+    @error_10frame_2nd_3rd_roll = 0
     @pin = DownPinNumber.new(@down_pins)
   end
   
-  def validate_down_pins
-    #@errors_pins_not_number = [nil]
-    #@errors_pins_over_10 = [nil]
-    #@error_3rd_roll_10frame = 0
-    #@errors_10frame_2nd_3rd_roll = 0
+  def count_errors
+    error_count = 0
 
-    validate_pin_is_number_low_10
+    error_count += validate_pin_is_number_low_10
     
-    validate_pin_number_sum_is_low_10_each_frame
+    error_count += validate_pin_number_sum_is_low_10_each_frame
 
-    validate_3rd_roll_10frame_is_0_unless_strike_or_spare
+    error_count += validate_3rd_roll_10frame_is_0_unless_strike_or_spare
 
-    validate_2nd_3rd_roll_sum_10frame_is_low_10
+    error_count += validate_2nd_3rd_roll_sum_10frame_is_low_10
     
-    return @errors_pins_not_number.size + @errors_pins_over_10.size + @error_3rd_roll_10frame + @errors_10frame_2nd_3rd_roll
-  end
-
-  def error?(g)
-    return g
+    return error_count
   end
 
   #倒したピン数の入力値は半角数字で０～１０でないといけない
@@ -44,6 +36,7 @@ class Validation
         @errors_pins_not_number << i
       end
     end
+    return @errors_pins_not_number.size - 1
   end
 
   #各フレームの1投目と2投目のピン数の和は10以下でないといけない（10フレーム目は例外がある）
@@ -55,6 +48,7 @@ class Validation
         end
       end
     end
+    return @errors_pins_over_10.size - 1
   end
 
   #10フレーム目の3投目はストライクかスペアでないと入力できない
@@ -64,15 +58,17 @@ class Validation
         @error_3rd_roll_10frame = 1
       end
     end
+    return @error_3rd_roll_10frame
   end
 
   #10フレーム目がストライクの時は2投目と3投目の和は10以下でないといけない（2投目がストライクの時は例外）
   def validate_2nd_3rd_roll_sum_10frame_is_low_10
     if @pin.strike?(10) && @pin.get_second_roll(10) != 10
       if @pin.get_second_roll(10) + @pin.get_third_roll(10) > 10
-          @errors_10frame_2nd_3rd_roll = 1
+          @error_10frame_2nd_3rd_roll = 1
       end
     end
+    return @error_10frame_2nd_3rd_roll
   end
         
 end
