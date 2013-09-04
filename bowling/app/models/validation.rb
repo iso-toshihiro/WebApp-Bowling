3eaa9ff850bcfd14edcda8c61dc66a3c0b1befc8
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-04 11:31:19 +0900

    リファクタリング、メソッド名変更、クラスDownPinNumber導入

--- bowling/app/models/validation.rb
+++ bowling/app/models/validation.rb
@@ -9,26 +9,27 @@ class Validation
   def initialize(down_pins)
     @down_pins                   = []
     @down_pins                   = down_pins
-    @errors_pins_not_number      = []
-    @errors_pins_over_10         = []
+    @errors_pins_not_number      = [nil]
+    @errors_pins_over_10         = [nil]
     @error_3rd_roll_10frame      = 0
     @errors_10frame_2nd_3rd_roll = 0
+    @pin = DownPinNumber.new(@down_pins)
   end
   
   def validate_down_pins
-    @errors_pins_not_number = [nil]
-    @errors_pins_over_10 = [nil]
-    @error_3rd_roll_10frame = 0
-    @errors_10frame_2nd_3rd_roll = 0
+    #@errors_pins_not_number = [nil]
+    #@errors_pins_over_10 = [nil]
+    #@error_3rd_roll_10frame = 0
+    #@errors_10frame_2nd_3rd_roll = 0
 
-    validate_pin_is_number
+    validate_pin_is_number_low_10
+    
+    validate_pin_number_sum_is_low_10_each_frame
 
-    validate_pin_low_10
+    validate_3rd_roll_10frame_is_0_unless_strike_or_spare
 
-    validate_3rd_roll_10frame
-
-    validate_10frame_2nd_3rd_roll
-
+    validate_2nd_3rd_roll_sum_10frame_is_low_10
+    
     return @errors_pins_not_number.size + @errors_pins_over_10.size + @error_3rd_roll_10frame + @errors_10frame_2nd_3rd_roll
   end
 
@@ -36,7 +37,8 @@ class Validation
     return g
   end
 
-  def validate_pin_is_number
+  #倒したピン数の入力値は半角数字で０～１０でないといけない
+  def validate_pin_is_number_low_10
     @down_pins.each.with_index do |pins, i|
       unless /^\d$/ =~ pins || pins == '10'
         @errors_pins_not_number << i
@@ -44,32 +46,33 @@ class Validation
     end
   end
 
-  def validate_pin_low_10
+  #各フレームの1投目と2投目のピン数の和は10以下でないといけない（10フレーム目は例外がある）
+  def validate_pin_number_sum_is_low_10_each_frame
     1.upto(10) do |frame|
-      if  @down_pins[2*frame-2].to_i + @down_pins[2*frame-1].to_i > 10
-        unless frame == 10 && @down_pins[2*frame-2].to_i == 10
+      if @pin.get_first_roll(frame) + @pin.get_second_roll(frame) > 10
+        unless frame == 10 && @pin.strike?(10)
           @errors_pins_over_10 << frame
         end
       end
     end
   end
 
-  def validate_3rd_roll_10frame
-    if @down_pins[20].to_i > 0
-      unless @down_pins[18].to_i == 10 || @down_pins[18].to_i + @down_pins[19].to_i == 10
+  #10フレーム目の3投目はストライクかスペアでないと入力できない
+  def validate_3rd_roll_10frame_is_0_unless_strike_or_spare
+    if @pin.get_third_roll(10) > 0
+      unless @pin.strike?(10) || @pin.spare?(10)
         @error_3rd_roll_10frame = 1
       end
     end
   end
 
-  def validate_10frame_2nd_3rd_roll
-    if @down_pins[18].to_i == 10 && @down_pins[19].to_i != 10
-      if  @down_pins[19].to_i + @down_pins[20].to_i > 10
+  #10フレーム目がストライクの時は2投目と3投目の和は10以下でないといけない（2投目がストライクの時は例外）
+  def validate_2nd_3rd_roll_sum_10frame_is_low_10
+    if @pin.strike?(10) && @pin.get_second_roll(10) != 10
+      if @pin.get_second_roll(10) + @pin.get_third_roll(10) > 10
           @errors_10frame_2nd_3rd_roll = 1
       end
     end
   end
         
-
-
 end
