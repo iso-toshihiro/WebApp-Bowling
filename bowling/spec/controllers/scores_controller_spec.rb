d4f18d39c09ccd367fcd46c4924b58f9e9a27b7f
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-09 18:27:20 +0900

    indexのテストの型を作成(scores_controller_spec.rb)

---
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -0,0 +1,14 @@
+#coding: utf-8
+require 'spec_helper'
+
+describe ScoresController do
+  describe 'GET index' do
+    before do
+      get :index
+    end
+    
+    it do
+    end
+    
+  end
+end
