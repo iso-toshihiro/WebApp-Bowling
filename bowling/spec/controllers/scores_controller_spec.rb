8b94f2aab1b6804b9e905c318fb506ffbdc7fda1
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-09 17:57:16 +0900

    score_controller_spec.rbにnewのテストの中身作成

--- bowling/spec/controllers/scores_controller_spec.rb
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -1,11 +1,24 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe ScoresController do
   describe "GET new" do
-
-    it do 
-
+    before do
+      get :new, from: ''
     end
+
+    it "プレイヤーの名前は’ユーザー’" do
+      assigns(:player_name).should == 'ユーザー'
+    end
+
+    it "日付は’2013-08-25 12:00:00’" do
+      assigns(:game_date).should == '2013-08-25 12:00:00'
+    end
+
+    it "テンプレートはnew" do
+      response.should render_template('new')
+    end
+    
   end
 
 end
