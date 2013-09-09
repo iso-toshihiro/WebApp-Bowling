8b3f1080ab80f46e8ea6db734b569c437c543e30
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-09 17:58:18 +0900

    score_controller_spec.rbにnewのテスト内容を追加

--- bowling/spec/controllers/scores_controller_spec.rb
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -21,5 +21,23 @@ describe ScoresController do
     
   end
 
+  describe "GET new from confirm" do
+    before do
+      down_pins = 
+      get :new, :from=>'confirm', :player_name=>'iso', :game_date=>'2014-11-03 05:33:12', :down_pins=>down_pins
+
+    it "プレイヤーの名前は’ユーザー’" do
+      assigns(:player_name).should == 'iso'
+    end
+
+    it "日付は’2014-11-03 05:33:12’" do
+      assigns(:game_date).should == '2014-11-03 05:33:12'
+    end
+
+    it "テンプレートはnew" do
+      response.should render_template('new')
+    end 
+  end
+
 end
 
