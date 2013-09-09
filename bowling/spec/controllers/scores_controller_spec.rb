fa6218d8e11d024294725b24638db3410bb4c889
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-09 20:19:22 +0900

    テスト追加、並び替えのテスト追加(scores_controller_spec.rb)

--- bowling/spec/controllers/scores_controller_spec.rb
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -2,13 +2,37 @@
 require 'spec_helper'
 
 describe ScoresController do
+  fixtures :scores
+
   describe 'GET index' do
+
     before do
       get :index
+      @record = scores(:exam, :xam)
+      
     end
     
-    it do
+    it 'スコア表示' do
+      #p assigns(:scores) #配列表示になっている
+      #p @record
+      assigns(:scores).should == @record
     end
     
   end
+
+  describe 'GET index sort' do
+
+    before do
+      get :index
+      @record = scores(:xam, :exam)
+      
+    end
+    
+    it 'スコア表示' do
+      assigns(:scores).should == @record
+    end
+    
+  end
+
+
 end
