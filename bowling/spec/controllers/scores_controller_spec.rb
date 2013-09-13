0a542619d4e25d1c47e798a8e64795a4751e0afc
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-13 13:55:08 +0900

    修正、日本語、レコード追加(scores_controller_spec.rb)

--- bowling/spec/controllers/scores_controller_spec.rb
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -1,4 +1,6 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
+# coding : utf-8
+
 require 'spec_helper'
 
 describe ScoresController do
@@ -8,12 +10,12 @@ describe ScoresController do
 
     before do
       get :index
-      @record = scores(:exam, :xam)
+      @record = scores(:sample_1, :sample_2, :sample_3)
       
     end
     
-    it 'スコア表示' do
-      #p assigns(:scores) #配列表示になっている
+    it 'インデックスが表示される' do
+      #p assigns(:scores) #配列で表示される
       #p @record
       assigns(:scores).should == @record
     end
@@ -23,12 +25,12 @@ describe ScoresController do
   describe 'GET index sort' do
 
     before do
-      get :index
-      @record = scores(:xam, :exam)
+      get :index, sort: 'sort_score'
+      @record = scores(:sample_2, :sample_3, :sample_1)
       
     end
     
-    it 'スコア表示' do
+    it 'スコア順に表示される' do
       assigns(:scores).should == @record
     end
     
