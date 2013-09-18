fef31e4f6cf081781e23488ffa59fd7f0bb9ff12
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-09-18 19:50:29 +0900

    indexテスト項目追加、スコア順並び替え

--- bowling/spec/controllers/scores_controller_spec.rb
+++ bowling/spec/controllers/scores_controller_spec.rb
@@ -11,30 +11,27 @@ describe ScoresController do
     before do
       get :index
       @record = scores(:sample_1, :sample_2, :sample_3)
-      
     end
     
-    it 'インデックスが表示される' do
-      #p assigns(:scores) #配列で表示される
-      #p @record
+    it 'インデックスが@scoresに渡されている' do
       assigns(:scores).should == @record
     end
     
   end
 
   describe 'GET index sort' do
-
-    before do
-      get :index, sort: 'sort_score'
+    it 'スコアが高い順にインデックスが@scoresに渡される' do
+      get :index, sort: 'sort_score_desc'
       @record = scores(:sample_2, :sample_3, :sample_1)
-      
+      assigns(:scores).should == @record
     end
-    
-    it 'スコア順に表示される' do
+
+    it 'スコアが低い順にインデックスが@scoresに渡される' do
+      get :index, sort: 'sort_score_asc'
+      @record = scores(:sample_1, :sample_3, :sample_2)
       assigns(:scores).should == @record
     end
     
   end
-
 
 end
